48f412b09b80b8668fabec057aabaaf376772a1c
Grace. W <grace.xuer.wu@gmail.com>
2022-02-10 15:12:05 +0100

    Hero Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        Hello world
-      </header>
+      <Home />
     </div>
   );
 }
---
+++ src/components/Hero.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "../styles/Hero.css";
+
+import Lottie from "lottie-react";
+import arrow from "../animations/arrow.json";
+
+export default function Hero() {
+  return (
+    <div className="Hero">
+      <h2>Hey I'm</h2>
+      <h1>Grace Wu</h1>
+      <h2>
+        An inquisitive multidisciplinary designer based in Lucerne, Switzerland
+        and also a Design Management student, who enjoys writing code and moving
+        pixels in the www.{" "}
+      </h2>
+      <br />
+      <div className="row">
+        <div className="col-9">
+          <h3>Scroll down to view my works</h3>
+        </div>
+        <div className="col-3">
+          <Lottie animationData={arrow} className="arrowAnimation" />
+        </div>
+      </div>
+    </div>
+  );
+}
