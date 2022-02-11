07390df0dc2a9e948aab26336a0c16363cdee350
Grace. W <grace.xuer.wu@gmail.com>
2022-02-11 18:06:40 +0100

    Footer component done

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,21 +1,24 @@
 import React from "react";
 import "../styles/Footer.css";
-
-//Phosphor icon import
-import { EnvelopeSimple, LinkedinLogo, GithubLogo } from "phosphor-react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
 
 export default function Footer() {
-    return (
-        <div className="container">
-            <div className="row">
-                <div className="col">
-                <h2>Designed and developed by Grace Wu.</h2>
-                <h2>Built with React. Hosted in Netlify</h2>
-                </div>
-                <div className="col">
-                <EnvelopeSimple size={32} />
-                </div>
-            </div>
-        </div>
-    )
+  return (
+    <div className="d-flex Footer">
+      <div className="left-col col-10">
+        <h2>Designed and developed by Grace Wu.</h2>{" "}
+        <h2>
+          Built with <strong>React</strong>. Hosted on Netlify.
+        </h2>
+      </div>
+      <div className="center-col"></div>
+      <div className="right-col col-2">
+        <FontAwesomeIcon icon={faEnvelope} className="fa-xl socialIcons" />
+        <FontAwesomeIcon icon={faLinkedin} className="fa-xl socialIcons" />
+        <FontAwesomeIcon icon={faGithub} className="fa-xl socialIcons" />
+      </div>
+    </div>
+  );
 }
--- src/components/ProjectComponent.js
+++ src/components/ProjectComponent.js
@@ -1,30 +1,36 @@
 import React from "react";
 import "../styles/FeaturedProjects.css";
 
-
 //Phosphor icon import
 import { Link, GithubLogo } from "phosphor-react";
 
 export default function ProjectComponent(props) {
-    return (
-        <div>
-            <img
-            src={props.image}
-            className="img-fluid text-center placeholder-img rounded projectImage"
-            alt=""
-          />
-          <div className="row">
-              <div className="col-10"> <h4>Weather App</h4></div>
-              <div className="col-1 linksLogo"><Link size={30} /></div>
-              <div className="col-1 linksLogo"><GithubLogo size={30} /></div>
-          </div>
-          <div className="projectDescription">
-            Weather application coded with Openweathermap API integration for
-            live weather information. Features a day and night background tuned
-            to your local time
-          </div>
-          <span className="projectSoftwares">Javascript</span>
-          <span className="projectSoftwares">Bootstrap</span>
+  return (
+    <div>
+      <img
+        src={props.image}
+        className="img-fluid text-center placeholder-img rounded projectImage"
+        alt=""
+      />
+      <div className="row">
+        <div className="col-10">
+          {" "}
+          <h4>Weather App</h4>
         </div>
-    )
+        <div className="col-1 linksLogo">
+          <Link size={30} />
+        </div>
+        <div className="col-1 linksLogo">
+          <GithubLogo size={30} />
+        </div>
+      </div>
+      <div className="projectDescription">
+        Weather application coded with Openweathermap API integration for live
+        weather information. Features a day and night background tuned to your
+        local time
+      </div>
+      <span className="projectSoftwares">Javascript</span>
+      <span className="projectSoftwares">Bootstrap</span>
+    </div>
+  );
 }
