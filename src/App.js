7fd224b86ba5fad6dbec05bd99820f6c6162ac2e
Grace. W <grace.xuer.wu@gmail.com>
2022-02-12 21:12:47 +0100

    React router implemented

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import FeaturedProjects from './components/FeaturedProjects';
 import Experience from './components/Experience';
 import Education from './components/Education';
 import Footer from './components/Footer';
+import Nav from './components/Nav';
+import About from './components/About';
+
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
---
+++ src/components/Nav.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../styles/Nav.css";
+
+export default function Nav() {
+  return (
+    <nav>
+      <h3>Logo</h3>
+      <ul>
+        <li>Projects</li>
+        <li>About</li>
+      </ul>
+    </nav>
+  );
+}
