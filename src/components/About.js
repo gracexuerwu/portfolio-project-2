5db833d71c43404b6a68e1e1439805a1f36a6950
Grace. W <grace.xuer.wu@gmail.com>
2022-02-14 21:13:49 +0100

    Styled Nav bar, about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,23 @@
 import React from "react";
+import "../styles/About.css";
+// import CVPhoto from "../images/CV_photo.png";
+import CVPhoto from "../images/CV_photo1.jpeg";
 
 export default function About() {
-    return (
-        <h1>About page</h1>
-    )
+  return (
+    <div className="About">
+      <div className="row">
+        <div className="col">
+          <h1>Hi I’m Grace Wu, interfacer designer &amp; developer.</h1>
+        </div>
+        <div className="col">
+        <img
+        src={CVPhoto}
+        className="CVphoto img-fluid"
+        alt=""
+      />
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,27 +1,91 @@
 import React from "react";
 import "../styles/Nav.css";
 import { NavLink } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMoon } from "@fortawesome/free-solid-svg-icons";
+import { House } from "phosphor-react";
 
 export default function Nav() {
   return (
-    <nav>
-      <NavLink to="/">
-        <h3>GW</h3>
-      </NavLink>
-      <ul>
-        <NavLink to="/">
-          {" "}
-          <li>Projects</li>
-        </NavLink>
-        <NavLink to="/about">
-          {" "}
-          <li>Contact</li>
-        </NavLink>
-        <NavLink to="/about">
-          {" "}
-          <li>About</li>
-        </NavLink>
-      </ul>
-    </nav>
+    // <nav>
+    //   <NavLink to="/">
+    //     <h3>GW</h3>
+    //   </NavLink>
+    //   <ul>
+    //     <NavLink to="/">
+    //       {" "}
+    //       <li>Projects</li>
+    //     </NavLink>
+    //     <NavLink to="/about">
+    //       {" "}
+    //       <li>Contact</li>
+    //     </NavLink>
+    //     <NavLink to="/about">
+    //       {" "}
+    //       <li>About</li>
+    //     </NavLink>
+    //   </ul>
+    // </nav>
+
+      <nav
+        className="navbar fixed-top navbar-expand-lg navbar-light"
+        style={{ color: "#0000" }}
+      >
+        <div className="container-fluid">
+          <a className="navbar-brand navbar-font" href="/" title="Homepage">
+          <House size={32} /></a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div
+            className="collapse navbar-collapse justify-content-end"
+            id="navbarNavAltMarkup"
+          >
+            <div className="navbar-nav">
+              <NavLink
+                exact
+                activeClassName="active"
+                to="/"
+                className="nav-link navbar-font mx-2"
+                aria-current="page"
+                title="Projects"
+              >
+                Projects
+              </NavLink>
+              <NavLink
+                to="/about"
+                className="nav-link navbar-font mx-2"
+                title="About"
+              >
+                Contact
+              </NavLink>
+              <NavLink
+                activeClassName="active"
+                to="/about"
+                className="nav-link navbar-font mx-2"
+                title="About"
+              >
+                About
+              </NavLink>
+              <NavLink
+                activeClassName="active"
+                to="/about"
+                className="nav-link navbar-font mx-2"
+                title="About"
+              >
+                <FontAwesomeIcon  icon={faMoon} className="fa-lg" />
+              </NavLink>
+            </div>
+          </div>
+        </div>
+      </nav>
   );
 }
