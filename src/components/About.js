ad390a8a7a815be3366d3fa0ef3d9537e2e3f3d6
Grace. W <grace.xuer.wu@gmail.com>
2022-02-16 08:21:18 +0100

    About comp

--- src/components/About.js
+++ src/components/About.js
@@ -7,17 +7,29 @@ export default function About() {
   return (
     <div className="About">
       <div className="row">
-        <div className="col">
-          <h1>Hi I’m Grace Wu, interfacer designer &amp; developer.</h1>
+        <div className="col-7">
+          <h1>Hi I’m Grace Wu, </h1>
+          <h1>interfacer designer &amp; developer.</h1>
+          <div className="about-text">
+            <p>
+              I’m a self-taught front-end interface developer, and am currently
+              pursuing a BA in Product and industrial design, seeking to create
+              human-centred design for and with people.
+              When the internet is down you can find me frolicking in the woods
+              or alps, cooking or reading a book.{" "}
+            </p>
+            <p>
+              I am open to internships and full-time positions in the front-end
+              development or service design field.
+            </p>
+          </div>
         </div>
-        <div className="col">
-        <img
-        src={CVPhoto}
-        className="CVphoto img-fluid"
-        alt=""
-      />
+        <div className="col-1"></div>
+        <div className="col-4">
+          <img src={CVPhoto} className="CVphoto img-fluid" alt="" />
         </div>
       </div>
     </div>
+    
   );
 }
