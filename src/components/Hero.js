71b4ca7a443d576b99dc4b68064c19a4d28bdc74
Grace. W <grace.xuer.wu@gmail.com>
2022-03-05 21:05:19 +0100

    Hero revamp

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,25 +4,28 @@ import "../styles/Hero.css";
 import Lottie from "lottie-react";
 import arrow from "../animations/arrow.json";
 
+//Phosphor icon import
+import { MapPinLine } from "phosphor-react";
+
 export default function Hero() {
   return (
     <div className="Hero">
-      <h3>Hey I'm</h3>
-      <h1>Grace Wu</h1>
+      <h1>
+        Hey I'm <span className="bold">Grace Wu</span>
+      </h1>
       <h2>
-        An <span>inquisitive multidisciplinary designer</span> &amp; Design Management student based in Lucerne, Switzerland. 
-        <br /> Who enjoys writing code and moving
-        pixels in the www.{" "}
+        An <span className="bold">inquisitive multidisciplinary designer</span>{" "}
+        based in   <MapPinLine size={50} /> Lucerne, Switzerland.
+        <br /> Who enjoys writing code and moving pixels in the www.{" "}
       </h2>
       <br />
-      <div className="row">
-        <div className="col-9">
-          <h4>Scroll down to view my works</h4>
-        </div>
+      <h4>What i do ↓</h4>
+      {/* <div className="row">
+        <div className="col-9"></div>
         <div className="col-3">
           <Lottie animationData={arrow} className="arrowAnimation" />
         </div>
-      </div>
+      </div> */}
     </div>
   );
 }
