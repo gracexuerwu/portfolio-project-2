d5d4fc5c417372372f9184d5bb9450273c0f5765
Guobi Wu <guobi.wu@autodesk.com>
2022-03-06 16:24:17 +0800

    fix margin and add sun icon

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,7 +2,7 @@ import React from "react";
 import "../styles/Nav.css";
 import { NavLink } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faMoon } from "@fortawesome/free-solid-svg-icons";
+import { faMoon, faSun } from "@fortawesome/free-solid-svg-icons";
 import { House } from "phosphor-react";
 
 export default function Nav(props) {
@@ -27,14 +27,14 @@ export default function Nav(props) {
     //   </ul>
     // </nav>
 
-      <nav
-        className="navbar navbar-customclass fixed-top navbar-expand-lg navbar-light"
-        style={{ color: "#0000" }}
-      >
-        <div className="container-fluid">
-          <a className="navbar-brand navbar-font" href="/" title="Homepage">
+    <nav
+      className="navbar navbar-customclass fixed-top navbar-expand-lg navbar-light"
+      style={{ color: "#0000" }}
+    >
+      <div className="container-fluid">
+        <a className="navbar-brand navbar-font" href="/" title="Homepage">
           <House size={32} /></a>
-          <button
+        <button
           className="navbar-toggler"
           type="button"
           data-bs-toggle="collapse"
@@ -75,18 +75,18 @@ export default function Nav(props) {
             >
               Contact
             </NavLink>
-            
+
             <NavLink
               activeClassName="active"
               to="#"
               className="nav-link navbar-font mx-2"
               title="Fake"
             >
-              <FontAwesomeIcon onClick={props.onClick} icon={faMoon} className="fa-lgn theme-toggle" />
+              <FontAwesomeIcon onClick={props.onClick} icon={props.theme === 'light' ? faSun : faMoon} className="fa-lgn theme-toggle" />
             </NavLink>
-            </div>
           </div>
         </div>
-      </nav>
+      </div>
+    </nav>
   );
 }
