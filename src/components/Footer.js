ee554f9ecb9a0473163682e599d6523f17a5c4a7
Grace. W <grace.xuer.wu@gmail.com>
2022-03-08 21:00:18 +0100

    900px: footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,13 +8,13 @@ export default function Footer() {
   return (
     <div className="Footer">
       <div className="row">
-      <div className="col-7 col-md-9">
+      <div className="col-7 col-md-8">
         <h2>Designed and developed by <strong>Grace Wu</strong>.</h2>{" "}
         <h2>
           Built with <strong>React</strong>. Hosted on Netlify.
         </h2>
       </div>
-      <div className="col-5 col-md-3">
+      <div className="col-5 col-md-4">
         <FontAwesomeIcon icon={faEnvelope} className="fa-xl socialIcons" />
         <FontAwesomeIcon icon={faLinkedin} className="fa-xl socialIcons" />
         <FontAwesomeIcon icon={faGithub} className="fa-xl socialIcons" />
