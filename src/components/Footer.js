a3fa95912e76097f6baa56489a39c1c207adfd36
Grace. W <grace.xuer.wu@gmail.com>
2022-03-09 09:41:59 +0100

    update 1100px

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,14 +7,14 @@ import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
 export default function Footer() {
   return (
     <div className="Footer">
-      <div className="row">
-      <div className="col-7 col-md-8">
+      <div className="flex-container space-between">
+      <div className="flex-item-left">
         <h2>Designed and developed by <strong>Grace Wu</strong>.</h2>{" "}
         <h2>
           Built with <strong>React</strong>. Hosted on Netlify.
         </h2>
       </div>
-      <div className="col-5 col-md-4">
+      <div className="flex-item-right">
         <FontAwesomeIcon icon={faEnvelope} className="fa-xl socialIcons" />
         <FontAwesomeIcon icon={faLinkedin} className="fa-xl socialIcons" />
         <FontAwesomeIcon icon={faGithub} className="fa-xl socialIcons" />
