e86d27b3738d521514e6d1c6eb6aa34ad3e8c4d2
Grace. W <grace.xuer.wu@gmail.com>
2022-03-10 21:11:29 +0100

    footer edit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,17 +8,33 @@ export default function Footer() {
   return (
     <div className="Footer">
       <div className="flex-container space-between">
-      <div className="flex-item-left">
-        <h2>Designed and developed by <strong>Grace Wu</strong>.</h2>{" "}
-        <h2>
-          Built with <strong>React</strong>. Hosted on Netlify.
-        </h2>
-      </div>
-      <div className="flex-item-right">
-        <FontAwesomeIcon icon={faEnvelope} className="fa-xl socialIcons" />
-        <FontAwesomeIcon icon={faLinkedin} className="fa-xl socialIcons" />
-        <FontAwesomeIcon icon={faGithub} className="fa-xl socialIcons" />
-      </div>
+        <div className="flex-item-left">
+          <h2>
+            Designed and developed by <strong>Grace Wu</strong>.
+          </h2>{" "}
+          <h2>
+            Built with <strong>React</strong>. Hosted on Netlify.
+          </h2>
+        </div>
+        <div className="flex-item-right footer-social">
+          <a
+            href="mailto:grace.xuer.wu@gmail.com"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon icon={faEnvelope} className="fa-xl socialIcons" />
+          </a>
+          <a href="" target="_blank">
+            <FontAwesomeIcon icon={faLinkedin} className="fa-xl socialIcons" />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/grace-ade-wu/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon icon={faGithub} className="fa-xl socialIcons" />
+          </a>
+        </div>
       </div>
     </div>
   );
