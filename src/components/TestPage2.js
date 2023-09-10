c5615c14bd76eaa71be50660269efde76902aafd
Grace. W <grace.xuer.wu@gmail.com>
2023-09-10 22:24:55 +0200

    Progress on test page2

--- src/components/TestPage2.js
+++ src/components/TestPage2.js
@@ -1,11 +1,27 @@
 import React from "react";
-import "../styles/Play.css";
+import "../styles/TestPage2.css";
+import SchindlerHeroImage from "../images/Schindler_HeroImg.png";
 
 export default function TestPage2() {
-    return (
-        <div className="Play">
-            <h1>Hello World</h1>
-            <h2 className="flashing-text">📣 Coming soon 📣</h2>
+  return (
+    <div className="Play">
+      <img src={SchindlerHeroImage} className="schindler-hero" alt="" />
+      <h1>Title</h1>
+      <hr />
+      <div className="container">
+        <div className="row align-items-start">
+          <div className="col">Timeline</div>
+          <div className="col">My Role</div>
+          <div className="col">Collaboration</div>
+          <div className="col">Organization</div>
         </div>
-    );
+        <div className="row align-items-start">
+          <div className="col">1.5 months</div>
+          <div className="col">My Role</div>
+          <div className="col">Kaho Sakuta, Kevin Pardal</div>
+          <div className="col">Schindler AG</div>
+        </div>
+      </div>
+    </div>
+  );
 }
